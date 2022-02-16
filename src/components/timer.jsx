682cc55988f4fe74e6685f5b01d482c3ec65262f
umikami <ggoku02@gmail.com>
2022-02-16 14:35:25 -0500

    implemented option to count down or up from a given number

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,29 +1,53 @@
-import '../styles/Timer.css'
+import '../styles/Timer.css';
+
+let isGoingUp = true
+let counter = 0;
+
+let state = {
+	value: ''
+}
+
+let getValue = (val) =>{
+	state.value = val.target.value
+}
+
+const setTime = () => {
+    counter = state.value
+}
+
 
 let timeDivisor = (timeElap) => {
-    let newTime = String(timeElap)
-    let timeArr = []
+	let newTime = String(timeElap);
+	let timeArr = [];
 
-    timeArr = newTime.split("")
+	timeArr = newTime.split('');
 
-    return timeArr
+	return timeArr;
+};
 
-} 
+const Timer = () => {
+	isGoingUp ? counter++ : counter--
+	let timePassed = timeDivisor(counter);
 
-const Timer = ({timeElapsed}) => {
-    let timePassed = timeDivisor(timeElapsed)
-    
-    return <div className='timer'>
-        <i className="fa-regular fa-clock"></i>
-        <div className="counter">
-            {/* Left to right */}
-            <span className="firstField field">{timePassed[timePassed.length-6] || 0}</span>
-            <span className="secondField field">{timePassed[timePassed.length-5] || 0}</span>
-            <span className="thirdField field">{timePassed[timePassed.length-4] || 0}</span>
-            <span className="fourthField field">{timePassed[timePassed.length-3] || 0}</span>
-            <span className="fifthField field">{timePassed[timePassed.length-2] || 0}</span>
-            <span className="sixthField field">{timePassed[timePassed.length-1] || 0}</span>
-        </div>
-    </div>
-}
-export default Timer
+	return (
+		<div className="timer">
+			<i className="fa-regular fa-clock" />
+			<div className="counter">
+				{/* Left to right */}
+				<span className="firstField field">{timePassed[timePassed.length - 6] || 0}</span>
+				<span className="secondField field">{timePassed[timePassed.length - 5] || 0}</span>
+				<span className="thirdField field">{timePassed[timePassed.length - 4] || 0}</span>
+				<span className="fourthField field">{timePassed[timePassed.length - 3] || 0}</span>
+				<span className="fifthField field">{timePassed[timePassed.length - 2] || 0}</span>
+				<span className="sixthField field">{timePassed[timePassed.length - 1] || 0}</span>
+			</div>
+            <div className="counter-input">
+                <input type="number" className='timeInpt' onChange={getValue}/>
+                <button className='btn' type='button' onClick={setTime}>Change Time</button>
+                <button className='btn' type='button' onClick={()=>isGoingUp = true}>▲</button>
+                <button className='btn' type='button' onClick={()=>isGoingUp = false}>▼</button>
+            </div>
+		</div>
+	);
+};
+export default Timer;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Timer from './components/timer'
 
-let counter = 0
-
 const MainPage = () => {
   return <>
-    <Timer timeElapsed={counter}/>
+    <Timer/>
   </>
 }
 
 setInterval(()=>{
-  counter++
   ReactDOM.render(<MainPage/>,document.getElementById('root'));
 },1000)
 
